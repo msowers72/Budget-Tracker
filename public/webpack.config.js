b5acaf7b963337aae11a5cc76015b15a387a74f7
Mike Sowers <msowers72@yahoo.com>
2021-10-02 15:13:08 -0400

    restructured files

--- public/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  entry: "./public/index.js",
-  output: {
-    path: __dirname + "/dist",
-    filename: "app.bundle.js"
-  },
-  mode: "development"
-};
-module.exports = config;
