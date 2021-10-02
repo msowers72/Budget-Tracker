759afcc900185476097138ee9b0cd3f404428fdc
Mike Sowers <msowers72@yahoo.com>
2021-10-02 14:45:13 -0400

    updated webpack

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,49 +1,9 @@
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-
 const config = {
-  entry: {
-    app: './public/index.js',
-    
-  },
+  entry: "./public/index.js",
   output: {
-    path: __dirname + '/dist',
-    filename: 'app.bundle.js',
+    path: __dirname + "/dist",
+    filename: "app.bundle.js"
   },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
-  },
-  plugins: [
-    new WebpackPwaManifest({
-      fingerprints: false,
-      name: 'Newsy app',
-      short_name: 'Newsy',
-      description: 'An application that allows you to view different news articles and save your favorites.',
-      background_color: '#01579b',
-      theme_color: '#ffffff',
-      'theme-color': '#ffffff',
-      start_url: '/',
-      icons: [
-        {
-          src: path.resolve('assets/images/icons/android-chrome-192x192.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
-        },
-      ],
-    }),
-  ],
+  mode: "development"
 };
-
 module.exports = config;
